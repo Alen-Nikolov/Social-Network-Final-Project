39b3012bcab3a0f1c4fafb19aa83eadd75a30488
Ален Николов <alen.nikolov@imperiaonline.org>
2017-06-08 12:01:33 +0300

    jquery removed from friendRequest.js

--- public/javascripts/directives/friendRequests.js
+++ public/javascripts/directives/friendRequests.js
@@ -1,22 +1,23 @@
-app.directive('friendRequests', ["userService", function(userService) {
+app.directive('friendRequests', ["userService", function (userService) {
     return {
         restrict: 'E',
         scope: {
             data: '='
         },
         templateUrl: './javascripts/directives/friendRequests.htm',
-        link: function(scope, $element) {
+        link: function (scope) {
             var reqFriendId = scope.data._id;
             //  =============== CONFIRM FRIEND REQUEST =========
-            scope.confirmRequest = function($event) {
-                userService.confirmRequest(reqFriendId).then(function(data) {
-                    $($event.currentTarget).hide();
-                    $($event.currentTarget.parentNode).html('<p>Friend request accepted</p>');
+            scope.confirmRequest = function () {
+                userService.confirmRequest(reqFriendId).then(function (data) {
+                   scope.requestVisible = 1;
                 });
             };
             //  =============== REJECT FRIEND REQUEST =========
-            scope.rejectRequest = function($event) {
-                userService.rejectRequest(reqFriendId).then(function(data) {});
+            scope.rejectRequest = function () {
+                userService.rejectRequest(reqFriendId).then(function (data) {
+                    scope.requestVisible = 2;
+                });
             };
 
         }
