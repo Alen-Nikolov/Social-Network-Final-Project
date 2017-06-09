0fc62bad4643514a6225370d82b2b517f82788ec
Ален Николов <alen.nikolov@imperiaonline.org>
2017-06-09 10:32:20 +0300

    constants Pending, Accepted and Rejected added

--- public/javascripts/directives/friendRequests.js
+++ public/javascripts/directives/friendRequests.js
@@ -6,18 +6,21 @@ app.directive('friendRequests', ["userService", function (userService) {
         },
         templateUrl: './javascripts/directives/friendRequests.htm',
         link: function (scope) {
+            scope.PENDING = 1;
+            scope.ACCEPTED = 2;
+            scope.REJECTED = 3;
             var reqFriendId = scope.data._id;
-            scope.requestVisible = 'pending';
+            scope.requestVisible = scope.PENDING;
             //  =============== CONFIRM FRIEND REQUEST =========
             scope.confirmRequest = function () {
                 userService.confirmRequest(reqFriendId).then(function (data) {
-                    scope.requestVisible = 'accepted';
+                    scope.requestVisible = scope.ACCEPTED;
                 });
             };
             //  =============== REJECT FRIEND REQUEST =========
             scope.rejectRequest = function () {
                 userService.rejectRequest(reqFriendId).then(function (data) {
-                    scope.requestVisible = 'rejected';
+                    scope.requestVisible = scope.REJECTED;
                 });
             };
         }
