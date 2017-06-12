da8bcd390208e8e3355668f85ab256411e244e28
Ален Николов <alen.nikolov@imperiaonline.org>
2017-06-12 09:20:00 +0300

    constant made instead of magic number 20

--- public/javascripts/controllers/friendsController.js
+++ public/javascripts/controllers/friendsController.js
@@ -1,11 +1,12 @@
-app.controller('friendsController', ['$scope','$routeParams', 'friendRequestService', function($scope, $routeParams, friendRequestService) {
+app.controller('friendsController', ['$scope', '$routeParams', 'friendRequestService', function ($scope, $routeParams, friendRequestService) {
     var userId = $routeParams.userId;
+    var FRIENDS_TO_SHOW = 20;
     //============= LOAD ALL FRIENDS ================
-    friendRequestService.downloadFriends(userId).then(function(res) {
+    friendRequestService.downloadFriends(userId).then(function (res) {
         $scope.friends = res.data;
-        $scope.someFriends = $scope.friends.slice(0, 20);
-        $scope.loadMore = function() {
-            $scope.someFriends = $scope.friends.slice(0, $scope.someFriends.length + 20);
+        $scope.someFriends = $scope.friends.slice(0, FRIENDS_TO_SHOW);
+        $scope.loadMore = function () {
+            $scope.someFriends = $scope.friends.slice(0, $scope.someFriends.length + FRIENDS_TO_SHOW);
         };
     });
 }]);
