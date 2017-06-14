cd501b8d211ac75019cb3ec385ab794ac51f83a4
Ален Николов <alen.nikolov@imperiaonline.org>
2017-06-14 13:52:46 +0300

    chat working without JQuery

--- public/javascripts/controllers/chat-users.controller.js
+++ public/javascripts/controllers/chat-users.controller.js
@@ -1,7 +1,9 @@
-app.controller('chatUsersController', ['$scope', '$rootScope', '$http', 'delayService', 'userService', function ($scope, $rootScope, $http, delayService, userService) {
+app.controller('chatUsersController', ['$scope', '$rootScope', '$http', 'delayService', 'userService',
+    function ($scope, $rootScope, $http, delayService, userService) {
     // ============= SEARCH USER BY FULL NAME ================
     var TIMEOUT_ON_KEYPRESS_CHAT = 300;
-
+    var receiverId = null;
+    var socket = io.connect();
 
     function loadUsersByName(userName) {
         userService.getUsers(userName).then(function (res) {
@@ -26,9 +28,36 @@ app.controller('chatUsersController', ['$scope', '$rootScope', '$http', 'delaySe
     $scope.closeDivUsers = function () {
         $scope.showDivChatUsers = false;
     };
-    // ================= SHOW/HIDE CHAT WINDOW =================
-    $scope.hideShowChat = function () {
-        $('.slide-toggle').slideToggle();
+
+    $scope.startChat = function (friendId, chatFriendName) {
+        userService.getMessages(friendId).then(function (res) {
+            $scope.messages = res.data;
+            receiverId = friendId;
+            $scope.friendName = chatFriendName;
+        });
+    };
+    $scope.sendMessage = function () {
+        var text = $scope.messageText;
+        if (receiverId && text) {
+            var message = {
+                name: $rootScope.user.fullName,
+                picture: $rootScope.user.PROFILE_IMG_URL,
+                text: text,
+                senderId: $rootScope.user._id,
+                receiverId: receiverId
+            };
+            socket.emit('send message', message);
+        }
+        $scope.messageText = '';
+
+        socket.on("new message", function (data) {
+            var receiver = data.msg.receiverId;
+            var sender = data.msg.senderId;
+            if (receiver === $rootScope.user._id || sender === $rootScope.user._id) {
+                $scope.messages.push(data.msg);
+                $scope.$apply();
+            }
+        });
     };
 
 
--- public/javascripts/directives/chatUsers.js
+++ public/javascripts/directives/chatUsers.js
@@ -1,4 +1,4 @@
-app.directive('chatUsers', ['$rootScope', "userService", function($rootScope, userService) {
+app.directive('chatUsers', ['$rootScope', 'userService', function($rootScope, userService) {
     return {
         restrict: 'E',
         scope: {
@@ -7,16 +7,7 @@ app.directive('chatUsers', ['$rootScope', "userService", function($rootScope, us
         templateUrl: './javascripts/directives/chatUsers.htm',
         link: function(scope, $element) {
             scope.startChat = function($event) {
-                $("#chatDivScroll").scrollTop($("#chatDivScroll")[0].scrollHeight);
-                var friendId = scope.data._id;
-                var friendName = scope.data.fullName;
-                $('.btn-chat').attr('data-friendId', friendId);
-                $('.friendName').html('with <strong>' + friendName + '</strong>');
-                userService.getMessages(friendId).then(function(res) {
-                    $("div.listWithMessages").html("");
-                    scope.$parent.$parent.messages = res.data;
 
-                });
             }
         }
     };
