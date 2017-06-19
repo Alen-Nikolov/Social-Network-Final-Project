0fb5e1bf0009ce3f135dc8cf993a8f8a95baa96c
Ален Николов <alen.nikolov@imperiaonline.org>
2017-06-19 10:39:15 +0300

    naming convention for services

---
+++ public/javascripts/services/comment.service.js
@@ -0,0 +1,19 @@
+app.factory('commentService', function ($http, $rootScope) {
+    function Comments() {
+        this.comments = null;
+    }
+
+    Comments.prototype.downloadComments = function (postId) {
+        return this.comments = $http.get('/comments/' + postId);
+    };
+
+    Comments.prototype.addComment = function (comment) {
+        return $http.post('/comments/', comment);
+    };
+
+    Comments.prototype.changeLikeComment = function (comId) {
+        return $http.put('/comments/' + comId);
+    };
+
+    return new Comments();
+});
