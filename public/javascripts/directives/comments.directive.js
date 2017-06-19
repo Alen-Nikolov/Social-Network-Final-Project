4082f35751ff48b4b6f85b8da91b46eca5ae41af
Ален Николов <alen.nikolov@imperiaonline.org>
2017-06-19 15:11:18 +0300

    functions removed from inside if statement

--- public/javascripts/directives/comments.directive.js
+++ public/javascripts/directives/comments.directive.js
@@ -10,34 +10,34 @@ app.directive('comments', ["$rootScope", "commentService", function ($rootScope,
             var userId = $rootScope.user._id;
             var comId = scope.data._id;
 
+            /**
+             * for every comment checks if the current user has liked it already and changes color of like button
+             */
+            scope.isLikedComment = function () {
+                if (scope.data.likes.indexOf(userId) === -1) {
+                    scope.isLiked = false;
+                } else {
+                    scope.isLiked = true;
+                }
+            };
+
+            /**
+             * If comment is already liked - unlike it, if comment is not liked yet - like it
+             */
+            scope.changeLikeComment = function () {
+                commentService.changeLikeComment(comId).then(function (res) {
+                    if (res.data[0].likes.indexOf(userId) === -1) {
+                        scope.data.likes.push(userId);
+                        scope.isLiked = true;
+                    } else {
+                        scope.data.likes.splice(scope.data.likes.indexOf(userId), 1);
+                        scope.isLiked = false;
+                    }
+                });
+            };
 
             if (comments.length !== 0) {
-                /**
-                 * for every comment checks if the current user has liked it already and changes color of like button
-                 */
-                scope.isLikedComment = function () {
-                    if (scope.data.likes.indexOf(userId) === -1) {
-                        scope.isLiked = false;
-                    } else {
-                        scope.isLiked = true;
-                    }
-                };
                 scope.isLikedComment();
-
-                /**
-                 * If comment is already liked - unlike it, if comment is not liked yet - like it
-                 */
-                scope.changeLikeComment = function () {
-                    commentService.changeLikeComment(comId).then(function (res) {
-                        if (res.data[0].likes.indexOf(userId) === -1) {
-                            scope.data.likes.push(userId);
-                            scope.isLiked = true;
-                        } else {
-                            scope.data.likes.splice(scope.data.likes.indexOf(userId), 1);
-                            scope.isLiked = false;
-                        }
-                    });
-                }
             }
         }
 
